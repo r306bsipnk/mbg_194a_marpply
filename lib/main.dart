846e2cc17ac3c5c288b28e2d72ab052d90bdaff2
melkaandriana <163995616+melkaandriana@users.noreply.github.com>
2024-05-20 12:29:58 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,46 @@
-import 'package:flutter/material.dart';
-import 'login_screen.dart'; // Import view controller
-import 'splash_screen.dart'; // Import splash screen
-import 'dashboard_screen.dart'; // Import dashboard screen
 
-void main() => runApp(MyApp());
+
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:marpply/lupapassword.dart';
+import 'screens/lupapassword.dart';
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplikasi Login',
+      title: 'Aplikasi Saya',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreen(), // Menampilkan splash screen terlebih dahulu
-      routes: {
-        '/login': (context) => LoginScreen(),
-        '/dashboard': (context) => DashboardScreen(),
-      },
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Beranda'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => LupaPasswordPage()),
+            );
+          },
+          child: Text('Lupa Password'),
+        ),
+      ),
     );
   }
 }
